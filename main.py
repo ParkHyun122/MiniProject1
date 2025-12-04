fc7df9cd2c40c0bab0ce7684937659d05432e3e2
ParkHyun122 <parkhyun122@gmail.com>
2025-12-04 14:46:32 -0500

    final

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-def mergeSort():
+import random
+from collections import deque
+def mergeSort(x):
+
+    if len(x) <= 1:
+        return x
     
+    halfway = len(x)//2
+
+    leftArray = mergeSort(x[:halfway])
+    rightArray = mergeSort(x[halfway:])
+
+    newArray = []
+
+    left = deque(leftArray)
+    right = deque(rightArray)
+
+    while left and right:
+        newArray.append(left.popleft() if left[0] <= right[0] else right.popleft())
+
+    newArray.extend(right)
+    newArray.extend(left)
+
+    return newArray
+
+newArray = []
+length = int(input("Enter array size: "))
+
+for i in range(length):
+    newArray.append(i+1)
+
+random.shuffle(newArray)
+
+print("before sort:", newArray)
+
+print("after sort:", mergeSort(newArray))
